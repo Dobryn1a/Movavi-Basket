7ca7cd8ccef775863db2fc9895732e87b83a67b1
Dobrynia <movist00@gmail.com>
2019-05-15 22:48:23 +0700

    Верстка страницы

--- gulpfile.js
+++ gulpfile.js
@@ -1,3 +1,76 @@
+// "use strict";
+
+// var gulp = require("gulp");
+// var sass = require("gulp-sass");
+// var plumber = require("gulp-plumber");
+// var postcss = require("gulp-postcss");
+// var autoprefixer = require("autoprefixer");
+// var rename = require("gulp-rename");
+// var csso = require("gulp-csso");
+// var imagemin = require("gulp-imagemin");
+// var del = require("del");
+// var server = require("browser-sync").create();
+
+// gulp.task("css", function () {
+//   return gulp.src("source/sass/style.scss")
+//     .pipe(plumber())
+//     .pipe(sass())
+//     .pipe(postcss([
+//       autoprefixer()
+//     ]))
+//     .pipe(gulp.dest("build/css"))
+//     .pipe(csso())
+//     .pipe(rename("style.min.css"))
+//     .pipe(gulp.dest("build/css"))
+//     .pipe(server.stream());
+// });
+
+// gulp.task("images", function () {
+//   return gulp.src("source/img/**/*.{png, jpg}")
+//     .pipe(imagemin([
+//       imagemin.optipng({optimizationLevel: 3}),
+//       imagemin.jpegtran({progressive: true}),
+//     ]))
+//     .pipe(gulp.dest("build/img"));
+// });
+
+// gulp.task("clean", function () {
+//   return del("build");
+// });
+
+// gulp.task("copy", function () {
+//   return gulp.src([
+//     "source/fonts/*.{woff,woff2}",
+//     "source/img/**",
+//     "source/js/**",
+//     "source/*.html"
+//   ], {
+//     base: "source"
+//   })
+//   .pipe(gulp.dest("build"));
+// });
+
+// gulp.task("server", function () {
+//   server.init({
+//     server: "build/", // "build/"
+//     notify: false,
+//     open: true,
+//     cors: true,
+//     ui: false
+//   });
+
+//   gulp.watch("source/sass/**/*.{scss,sass}", gulp.series("css"));
+//   gulp.watch("source/*.html").on("change", server.reload);
+// });
+
+// gulp.task("build", gulp.series(
+//   "clean",
+//   "copy",
+//   "css"
+// ));
+
+// gulp.task("start", gulp.series("server"));
+
 "use strict";
 
 var gulp = require("gulp");
@@ -5,10 +78,6 @@ var sass = require("gulp-sass");
 var plumber = require("gulp-plumber");
 var postcss = require("gulp-postcss");
 var autoprefixer = require("autoprefixer");
-var rename = require("gulp-rename");
-var csso = require("gulp-csso");
-var imagemin = require("gulp-imagemin");
-var del = require("del");
 var server = require("browser-sync").create();
 
 gulp.task("css", function () {
@@ -18,41 +87,13 @@ gulp.task("css", function () {
     .pipe(postcss([
       autoprefixer()
     ]))
-    .pipe(gulp.dest("build/css"))
-    .pipe(csso())
-    .pipe(rename("style.min.css"))
-    .pipe(gulp.dest("build/css"))
+    .pipe(gulp.dest("source/css"))
     .pipe(server.stream());
-});
-
-gulp.task("images", function () {
-  return gulp.src("source/img/**/*.{png, jpg}")
-    .pipe(imagemin([
-      imagemin.optipng({optimizationLevel: 3}),
-      imagemin.jpegtran({progressive: true}),
-    ]))
-    .pipe(gulp.dest("build/img"));
-});
-
-gulp.task("clean", function () {
-  return del("build");
-});
-
-gulp.task("copy", function () {
-  return gulp.src([
-    "source/fonts/*.{woff,woff2}",
-    "source/img/**",
-    "source/js/**",
-    "source/*.html"
-  ], {
-    base: "source"
-  })
-  .pipe(gulp.dest("build"));
 });
 
 gulp.task("server", function () {
   server.init({
-    server: "build/",
+    server: "source/",
     notify: false,
     open: true,
     cors: true,
@@ -63,10 +104,4 @@ gulp.task("server", function () {
   gulp.watch("source/*.html").on("change", server.reload);
 });
 
-gulp.task("build", gulp.series(
-  "clean",
-  "copy",
-  "css"
-));
-
-gulp.task("start", gulp.series("build", "server"));
+gulp.task("start", gulp.series("css", "server"));
